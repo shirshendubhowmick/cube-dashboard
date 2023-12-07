d3778b06b7502bca879551c65bbc116ab3b00c3c
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2023-12-08 02:02:13 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "Open Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
