d9babd772f7bc1c054acb4fd71c0ac7bf8e8dcc4
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2023-12-10 03:34:19 +0530

    removed extension

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import App from "./App.tsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
