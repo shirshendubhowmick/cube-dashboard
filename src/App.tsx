0491838b2505c315a7134fd1afd7ccd534d592ec
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2023-12-10 03:34:47 +0530

    WIP visualizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { useCubeQuery } from "@cubejs-client/react";
-import cubejs from "@cubejs-client/core";
-
-const cubejsApi = cubejs(CUBEJS_TOKEN, {
-  apiUrl: CUBEJS_API_URL,
-});
+import Visualizations from "./pages/Visualizations/Visualizations";
 
 function App() {
-  const { resultSet, isLoading, error, progress } = useCubeQuery(
-    {
-      measures: ["Orders.count"],
-      dimensions: ["Orders.createdAt.month"],
-    },
-    {
-      cubejsApi,
-    },
-  );
-
-  console.log(resultSet, isLoading, error, progress);
-
   return (
     <>
-      <h1>Hello world</h1>
+      <Visualizations />
     </>
   );
 }
