c79107bce1f45cf7b65c1313d861d6183edcb207
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2023-12-10 12:39:30 +0530

    added visualization toggle

--- src/pages/Visualizations/Visualizations.tsx
+++ src/pages/Visualizations/Visualizations.tsx
@@ -1,7 +1,27 @@
+import { useCallback, useState } from "react";
+
 import Map from "./components/Map/Map";
+import Charts from "./components/Charts/Charts";
 
 function Visualizations() {
-  return <Map />;
+  const [isChartView, setIsChartView] = useState(true);
+
+  const onClick = useCallback(() => {
+    setIsChartView((prev) => !prev);
+  }, []);
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={onClick}
+        className="mb-16 rounded border border-solid border-gray-800 bg-gray-800 px-9 py-2 text-white"
+      >
+        {isChartView ? "Map view" : "Chart view"}
+      </button>
+      {isChartView ? <Charts /> : <Map />}
+    </>
+  );
 }
 
 export default Visualizations;
