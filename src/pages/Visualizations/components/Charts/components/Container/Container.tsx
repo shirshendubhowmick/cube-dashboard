f32964cb8b995cb87d0c58049de2e8311653309d
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2023-12-10 14:45:31 +0530

    added configurable height

--- src/pages/Visualizations/components/Charts/components/Container/Container.tsx
+++ src/pages/Visualizations/components/Charts/components/Container/Container.tsx
@@ -1,6 +1,14 @@
-function Container(props: { children: React.ReactNode; label: string }) {
+export interface ContainerProps {
+  children: React.ReactNode;
+  label: string;
+  height?: number;
+}
+function Container(props: ContainerProps) {
   return (
-    <div className="inline-block h-[400px] w-full">
+    <div
+      className="mb-16 inline-block w-full"
+      style={{ height: props.height ?? 400 }}
+    >
       <p className="mb-4 text-lg font-medium">{props.label}</p>
       {props.children}
     </div>
